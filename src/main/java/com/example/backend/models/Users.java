e01b7ea12802e4ded7e767cabb3edba1ac4c6789
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-07 13:27:19 -0700

    added connection from spring app to sql server

--- src/main/java/com/example/backend/models/Users.java
+++ src/main/java/com/example/backend/models/Users.java
@@ -1,4 +1,52 @@
 package com.example.backend.models;
 
+
+import org.hibernate.annotations.Table;
+
+import javax.persistence.*;
+
+@Entity
+@Table(appliesTo = "users")
 public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String email;
+
+    public Users(Users copy) {
+        username = copy.username;
+        email = copy.email;
+    }
+
+    public Users() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
