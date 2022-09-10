6419c45ec57f5f38b17dffd56eeb05f5c3d8598a
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-10 12:46:23 -0700

    added cross origin notations to delete mapping

--- src/main/java/com/example/backend/controllers/UsersControllers.java
+++ src/main/java/com/example/backend/controllers/UsersControllers.java
@@ -3,16 +3,20 @@ package com.example.backend.controllers;
 import com.example.backend.models.Users;
 import com.example.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 public class UsersControllers {
     @Autowired
     private UserService userService;
+
 
     @PostMapping("/save")
     public String add(@RequestBody Users user) {
@@ -27,6 +31,7 @@ public class UsersControllers {
 
 
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("delete/{id}")
     public String deleteUser(@PathVariable Users id) {
         userService.deleteUsers(id);
