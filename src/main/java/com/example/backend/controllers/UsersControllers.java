c35691c8d89e73b4f959f424716800f8dc4cc17b
Nick Adams <nick.pappas.adams@gmail.com>
2022-09-10 19:04:01 -0700

    user controller refactor

--- src/main/java/com/example/backend/controllers/UsersControllers.java
+++ src/main/java/com/example/backend/controllers/UsersControllers.java
@@ -3,16 +3,14 @@ package com.example.backend.controllers;
 import com.example.backend.models.Users;
 import com.example.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UsersControllers {
     @Autowired
     private UserService userService;
